19610527369f57682b28e290707668cf37b5a0a2
Stephanie Lim <stephlim@uw.edu>
2017-12-03 20:06:39 -0800

    done with functionality

--- challenge9/src/Calculate.jsx
+++ challenge9/src/Calculate.jsx
@@ -13,13 +13,14 @@ class Calculate extends Component {
         return (
             <div>
                 <button ref="calculateButton" onClick={(e) => this.handleCalculateButton(e)} className="btn btn-success">Calculate Similarity!</button>
-                <h4>We are % confident that these two faces are similar.</h4>
+                <h4 ref="confidenceLevel">We are % confident that these two faces are similar.</h4>
             </div>
         );
     }
 
     handleCalculateButton(e) {
         e.preventDefault();
+        var componentObject = this;
         if (this.props.hasTwoPictures) {
             this.getFaceId(this.props.faceURL1, (result) => {
                 if (result !== null) {
@@ -33,7 +34,8 @@ class Calculate extends Component {
                     console.log(faceId2);
                 } 
                 this.verifyFace(faceId1, faceId2, (result2) => {
-                    console.log(result2);
+                    console.log(result2.confidence);
+                    componentObject.refs.confidenceLevel.textContent = "We are " + (Math.round(result2.confidence*100)) + "% confident these two faces are similar.";
                 })
             });
         }
